e26c10343c5ce173000d7d1badcafb7e2d11c855
Aan Nur Wahidi <aannurwahidi117@gmail.com>
2021-12-16 21:06:31 +0700

    Nerf gun

--- Assets/Scripts/Items/Weapon/Suntikan.cs
+++ Assets/Scripts/Items/Weapon/Suntikan.cs
@@ -6,14 +6,32 @@ public class Suntikan : MonoBehaviour
 {
     public Transform shootPoint;
     public SuntikanBullet suntikanBullet;
+    public bool canShoot = true;
+    private float delay = 1f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(suntikanBullet, shootPoint.position, shootPoint.rotation);
-            FindObjectOfType<AudioManager>().PlaySound("Gun_Shoot");
+            if (canShoot)
+            {
+                Shoot();
+                canShoot = false;
+                StartCoroutine(ShootDelay());
+            }
         }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(suntikanBullet, shootPoint.position, shootPoint.rotation);
+        FindObjectOfType<AudioManager>().PlaySound("Gun_Shoot");
+    }
+
+    private IEnumerator ShootDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        canShoot = true;
     }
 }
